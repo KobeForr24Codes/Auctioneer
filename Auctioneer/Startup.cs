b092d04515e1431f1da8d6bdc27b769e46f0e89a
Kobe Bryant Forrosuelo <kbpforrosuelo@outlook.com>
2018-03-30 21:41:38 +0800

    Add project files.

---
+++ Auctioneer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Auctioneer.Startup))]
+namespace Auctioneer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
